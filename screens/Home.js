020f2f079f64de1f41efe783b3c3b52c88db0e64
AtulPatle123 <atulpatlebgt@outlook.com>
2021-11-03 16:16:27 +0530

    searchBar ui design completed

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import HeaderTabs from '../components/HeaderTabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import SearchBar from './SearchBar';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{backgroundColor: '#eee' , flex: 1}}>
-      <View style={{backgroundColor: 'white' , padding : 15}}>
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 14}}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   );
